296c4c1cd2ea826da0c92f72d8631a2cfe8190b2
s2misha <s2misha4321@gmail.com>
2025-06-09 12:14:36 -0500

    Update opinion.php
    nuev

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "GSP EN LA NUBE",
+"autor" => "AGUILAR QUIÑONES ELQUIN",
+"resumen" => "Preguntas relacionadas
+¿Qué es el GSP y para qué sirve?
+La implementación del GSP, incluyendo las acciones y proyectos de gestión, puede servir para proteger o dañar los recursos de agua potable. Por ejemplo, las acciones de GSAs que podrían dañar el agua potable",
 ];
 ?>
