f72a502d95ab4f8bd3d10dbc0a35a49d4124b1d1
s2misha <s2misha4321@gmail.com>
2025-06-09 12:13:13 -0500

    Update nacional.php
    dos

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "RESCATE DE LA PILOTA ASHLEY VARGAS",
+"autor" => "Luis Aguilar",
+"resumen" => "Los restos de la piloto de la Fuerza Aérea del Perú (FAP), Ashley Stacy Vargas Mendoza han sido hallados la tarde de este jueves 5 de junio. Tras 16 días de intensa búsqueda, la alférez fue encontrada sin vida en el mar de Pisco a bordo de su aeronave KT-1P, de acuerdo a la información a la que accedió La República. Según el comunicado de la institución, se halló en el lecho marino el cuerpo de la aviadora aún sujeta a su asiento eyectable.",
 ];
 ?>
